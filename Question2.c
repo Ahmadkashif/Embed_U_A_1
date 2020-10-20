6cbb27ed5118908345d443be6933df99e25ee2dd
Ahmadkashif <ahmadkashif983@gmail.com>
2020-10-21 01:37:31 +0500

    final

--- Question2.c
+++ Question2.c
@@ -1,8 +1,28 @@
 #include <stdint.h>
 #include <stdio.h>
 #include "tm4c123gh6pm.h"
-
 #define RED_LED (*((volatile uint32_t *) 0x400253FC ))
+
+void initPortF1(void)
+{
+    //SET CLOCK FOR PORT F
+	SYSCTL_RCGCGPIO_R |= 0x00000020; 
+    //STABLIZATION
+  while((SYSCTL_PRGPIO_R & 0x00000020) == 0){
+
+  };
+  //ENABLE GPIO PORT F
+  GPIO_PORTF_LOCK_R = 0x4C4F434B;
+  //ALLOW CHANGES
+  GPIO_PORTF_CR_R = 0x02;
+  //DIRECTION OF PINS
+  GPIO_PORTF_DIR_R = 0x02;
+  //
+  GPIO_PORTF_PUR_R = 0x10; 
+  //DIGITAL ENABLE
+  GPIO_PORTF_DEN_R = 0x02; 
+}
+
 
 void quarterSecDelay(void)
 {
@@ -17,25 +37,6 @@ void quarterSecDelay(void)
     //STOP TIMER
 	NVIC_ST_CTRL_R = 0; 
 }
-
-void initPortF1(void)
-{
-    //SET CLOCK FOR PORT F
-	SYSCTL_RCGCGPIO_R |= 0x00000020; // activate clock for Port F
-    //STABLIZATION
-  while((SYSCTL_PRGPIO_R&0x00000020) == 0){};
-  //ENABLE GPIO PORT F
-  GPIO_PORTF_LOCK_R = 0x4C4F434B;
-  //ALLOW CHANGES
-  GPIO_PORTF_CR_R = 0x02;
-  //DIRECTION OF PINS
-  GPIO_PORTF_DIR_R = 0x02;
-  //
-  GPIO_PORTF_PUR_R = 0x10; 
-  //DIGITAL ENABLE
-  GPIO_PORTF_DEN_R = 0x02; 
-}
-
 
 int main()
 {
